c042ad82e471c191a12a7fd5ebbfc6cb9ca26ade
Emily Watkins and Kimberly Huynh <kimberly@>
2018-02-07 10:26:18 -0800

    add reader method, re-write test to take in two values.

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,15 +1,27 @@
-class RPS
-  def initialize(player1)
-    @player1 = player1
+class Player
+  def initialize(choice)
+    @choice = choice
+  end
+
+  def choice
+    @choice
   end
 
   def wins?(second)
-    # if @game == "rock" && second == "rock"
-    #   false
-    # elsif @game == "rock" && second == "paper"
-    #   false
-    # elsif
-    @player1 == "rock" && second == "scissors"
+    if @choice == "rock" && second == "rock"
+      false
+    elsif @choice == "rock" && second == "paper"
+      false
+    else @choice == "rock" && second == "scissors"
+      true
+    end
 
-   end
+    # if @player1 == "paper" && second == "scissors"
+    #   false
+    # elsif @player1 == "paper" && second == "paper"
+    #   false
+    # else @player1 == "paper" && second == "rock"
+    # end
+  end
+
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -5,14 +5,17 @@ require('rps')
 describe('#RPS') do
   describe('#wins') do
     it("returns true if rock is the object and scissors is the argument") do
-      player1 = RPS.new("rock")
-      expect(player1.wins?("scissors")).to(eq(true))
+      player1 = Player.new("rock")
+      player2 = Player.new("scissors")
+      expect(player1.wins?(player2.choice)).to(eq(true))
     end
 
     it("returns true or false for rock object") do
-      player1 = RPS.new("rock")
-      expect(player1.wins?("paper")).to(eq(false))
+      player1 = Player.new("rock")
+      player2 = Player.new("paper")
+      expect(player1.wins?(player2.choice)).to(eq(false))
     end
+
 
   end
 end
